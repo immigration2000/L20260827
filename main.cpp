fdaec2b4c69cacd15141d0441afc487136742df0
immigration2000 <immigration200057@gmail.com>
2026-08-27 09:13:19 +0900

    .

--- main.cpp
+++ main.cpp
@@ -1,42 +1,7 @@
 #include <iostream>
 
 using namespace std;
-class Character
-{
-public:
-	CharacterInfo CharacterInfo;
 
-	int Move();
-	int Attack();
-};
-
-class Player : public Character
-{
-
-};
-
-class Monster : public Character
-{
-	MonsterType MonsterType = Slime;
-};
-
-struct CharacterInfo
-{
-	int HP = 10;
-	int Attack = 10;
-	int Speed = 0;
-}; 
-enum MonsterType
-{
-	Slime,
-	Goblin,
-	WildBoar
-};
-enum job
-{
-	Warrior,
-	Thief
-};
 int main()
 {
 
